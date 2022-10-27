0661b9d9fe233a3f168ff1badd1db1015132f963
BeholderGit <beholderer@gmail.com>
2022-10-27 19:17:21 +0200

    base apparel v1 finished

---
+++ base-apparel-coming-soon/app.js
@@ -0,0 +1,37 @@
+const form = document.querySelector("form");
+const email = document.querySelector(".email");
+const error = document.querySelector(".error");
+const errorImg = document.querySelector(".error-img");
+
+form.addEventListener("input", function () {
+    error.innerHTML = "";
+    email.classList.remove("input-error");
+    errorImg.classList.remove("visible");
+});
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const input = document.querySelector(".email").value;
+    if (!input) {
+        errorImg.classList.add("visible");
+        email.classList.add("input-error");
+        error.innerHTML = "";
+        error.insertAdjacentText(
+            "beforeend",
+            "Email field is empty, please provide an email"
+        );
+    } else {
+        ValidateEmail(input);
+    }
+});
+
+function ValidateEmail(input) {
+    var validRegex =
+        /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!input.match(validRegex)) {
+        errorImg.classList.add("visible");
+        email.classList.add("input-error");
+        error.innerHTML = "";
+        error.insertAdjacentText("beforeend", "Please provide a valid email");
+        return false;
+    }
+}
