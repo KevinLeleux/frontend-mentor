398297427ab480efee667fb88ec3c36e5a0c2136
BeholderGit <beholderer@gmail.com>
2022-11-01 17:49:57 +0100

    signup form finished

--- newbie/intro-component-with-signup-form/app.js
+++ newbie/intro-component-with-signup-form/app.js
@@ -10,7 +10,10 @@ form.addEventListener("input", function () {
     for (let i = 0; i < input.length; i++) {
         if (input[i].value) {
             if (input[i].classList.contains("error")) {
-                input[i].classList.remove("error");
+                if (input[i].classList[0] != "email") {
+                    
+                }
+                    input[i].classList.remove("error");
                 const span = document.querySelector(
                     "." + input[i].classList[0] + "-span"
                 );
@@ -54,6 +57,7 @@ function validateEmail(emailCheck) {
         /^([A-Z|a-z|0-9](\.|_){0,1})+[A-Z|a-z|0-9]\@([A-Z|a-z|0-9])+((\.){0,1}[A-Z|a-z|0-9]){2}\.[a-z]{2,3}$/gm;
     if (!emailCheck.match(validRegex)) {
         const span = document.querySelector(".email-span");
+        span.innerHTML = "";
         span.insertAdjacentText("beforeend", "Looks like this is not an email");
         email.classList.add("error");
     }
