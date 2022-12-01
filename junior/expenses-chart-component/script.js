c6850d5a632e41df032e7880fd0dd0d6db8f4926
BeholderGit <beholderer@gmail.com>
2022-12-01 16:48:24 +0100

    .

--- junior/expenses-chart-component/script.js
+++ junior/expenses-chart-component/script.js
@@ -4,8 +4,8 @@ async function getData() {
         0: "sun",
         1: "mon",
         2: "tue",
-        3: "wed",
-        4: "thu",
+        4: "wed",
+        3: "thu",
         5: "fri",
         6: "sun",
     };
