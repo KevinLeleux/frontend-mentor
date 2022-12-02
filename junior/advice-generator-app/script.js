788fedad78d96fd099f4bc5a7b599d126b2c0a38
BeholderGit <beholderer@gmail.com>
2022-12-02 02:00:16 +0100

    advice generator finished challenge

--- junior/advice-generator-app/script.js
+++ junior/advice-generator-app/script.js
@@ -1,45 +1,57 @@
-const dice = document.querySelector(".dice");
 let temp = {};
+let quotes = {};
+let quotesArray = [];
+const dice = document.querySelector(".dice");
 const tempQuoteContainer = document.querySelector(".temp-quote-container");
-tempQuoteContainer.setAttribute("style", "z-index: -1;");
 
 dice.addEventListener("click", function () {
-    const tempQuoteContainer = document.querySelector(".temp-quote-container");
+    tempQuoteContainer.classList.remove("translate");
     tempQuoteContainer.setAttribute("style", "z-index: 1;");
-    fetchData();
-    tempQuoteContainer.classList.remove("translate"); // reset animation
-    void tempQuoteContainer.offsetWidth;
-    setTimeout(function () {
+    createCard();
+    setTimeout(() => {
         tempQuoteContainer.classList.add("translate");
-    }, 50);
-    setTimeout(function () {
+    }, 0);
+    setTimeout(() => {
         tempQuoteContainer.setAttribute("style", "z-index: -1;");
     }, 500);
 });
 
-async function fetchData() {
-    const quotesApi = await (
-        await fetch("https://api.adviceslip.com/advice")
+const initialize = async () => {
+    const quotesFetch = await (
+        await fetch("https://api.adviceslip.com/advice/search/e")
     ).json();
+    const arr = quotesFetch.slips;
+    for (let index = 0; index < arr.length; index++) {
+        quotes = Object.assign({
+            id: arr[index].id,
+            advice: arr[index].advice,
+            date: arr[index].date,
+        });
+        quotesArray.push(quotes);
+    }
+    createCard();
+};
+
+function createCard() {
+    let index = Math.floor(Math.random() * (quotesArray.length - 0) + 0);
     const h1 = document.querySelector(".title");
     h1.innerHTML = "";
-    h1.insertAdjacentText("beforeend", "Advice #" + quotesApi.slip.id);
+    h1.insertAdjacentText("beforeend", "Advice #" + quotesArray[index].id);
     const quote = document.querySelector(".quote");
     quote.innerHTML = "";
-    quote.insertAdjacentText("beforeend", quotesApi.slip.advice);
+    quote.insertAdjacentText("beforeend", quotesArray[index].advice);
 
-    setTimeout(function () {
-        const tempH1 = document.querySelector(".temp-title");
-        tempH1.innerHTML = "";
-        tempH1.insertAdjacentText("beforeend", "Advice #" + temp.id);
-        const tempQuote = document.querySelector(".temp-quote");
-        tempQuote.innerHTML = "";
-        tempQuote.insertAdjacentText("beforeend", temp.advice);
-    }, 800);
+    const tempH1 = document.querySelector(".temp-title");
+    tempH1.innerHTML = "";
+    tempH1.insertAdjacentText("beforeend", "Advice #" + temp.id);
+    const tempQuote = document.querySelector(".temp-quote");
+    tempQuote.innerHTML = "";
+    tempQuote.insertAdjacentText("beforeend", temp.advice);
+
     temp = {
-        id: quotesApi.slip.id,
-        advice: quotesApi.slip.advice,
+        id: quotesArray[index].id,
+        advice: quotesArray[index].advice,
     };
 }
 
-fetchData();
+initialize();
